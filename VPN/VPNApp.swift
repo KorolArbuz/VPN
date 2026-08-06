ab676c8c82485f322d9383c7caa9cf6571147452
KorolArbuz <denischizhov111@gmail.com>
2026-08-06 23:08:00 +0300

    Initial Commit

---
+++ VPN/VPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  VPNApp.swift
+//  VPN
+//
+//  Created by Denis Chizhov on 06.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct VPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
